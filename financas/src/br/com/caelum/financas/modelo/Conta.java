06852451b81767ec387f4f9bc4ccccf221955504
juniorhc <junior.jaava@gmail.com>
2018-09-10 20:20:51 +0100

    I've been studying Bidirection, lazy and eager loading

--- financas/src/br/com/caelum/financas/modelo/Conta.java
+++ financas/src/br/com/caelum/financas/modelo/Conta.java
@@ -1,9 +1,12 @@
 package br.com.caelum.financas.modelo;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 
 @Entity
@@ -17,6 +20,9 @@ public class Conta {
 	private String banco;
 	private String agencia;
 	private String numero;
+	
+	@OneToMany(mappedBy="conta")
+	private List<Movimentacao> movimentacoes;
 
 	public Integer getId() {
 		return id;
@@ -57,4 +63,14 @@ public class Conta {
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+
+	public List<Movimentacao> getMovimentacoes() {
+		return movimentacoes;
+	}
+
+	public void setMovimentacoes(List<Movimentacao> movimentacoes) {
+		this.movimentacoes = movimentacoes;
+	}
+	
+	
 }
